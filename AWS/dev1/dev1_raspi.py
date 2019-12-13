d85cbe8b9b4c6299a4cceb2e9d158645cca4bfe4
sagarambilpure <37987124+sagarambilpure@users.noreply.github.com>
2019-12-14 02:38:09 +0530

    raspi code khatam

--- AWS/dev1/dev1_raspi.py
+++ AWS/dev1/dev1_raspi.py
@@ -10,18 +10,37 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 #Setup Lock pin
-GPIO.setup(2, GPIO.OUT)
-GPIO.setup(3, GPIO.OUT)
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+lockers = {0:{"green_light":17,"red_light":27,"pin":22}, 1:{"green_light":5,"red_light":6,"pin":13}, 2:{"green_light":16,"red_light":20,"pin":21}}
+
+def change_led(slot,phone_status):
+    if phone_status == 'outside':
+        GPIO.output(lockers[slot]['green_light'],True)
+    else phone_status == 'inside':
+        GPIO.output(lockers[slot]['red_light'],True)
 '''
 
 def open_locker(slot):
     print('XXXXXXXXXXXXXXXXXXXXX')
+    #Remove When uploaded in raspi
+    # GPIO.output(lockers[slot]['pin'], True)
     print(slot,"Unlocked")
     print('XXXXXXXXXXXXXXXXXXXXX')
 
 def close_locker(slot):
     print('XXXXXXXXXXXXXXXXXXXXX')
+    #Remove When uploaded in raspi
+    # GPIO.output(lockers[slot]['pin'], True)
     print(slot,"Locked")
     print('XXXXXXXXXXXXXXXXXXXXX')
 
@@ -32,12 +51,14 @@ def customCallback(client, userdata, message):
         print(instruction['error_desc'])
         print('XXXXXXXXXXXXXXXXXXXXX')
     else:
+        phone_status = instruction['phone_status']
         action = instruction['action']
         slot = instruction['slot']
         if action == "open":
             open_locker(slot)
         elif action == "close":
             close_locker(slot)
+        change_led(slot,phone_status)
 
 host = 'a1wlltnsvntckz-ats.iot.ap-south-1.amazonaws.com'
 rootCAPath = 'root-CA.pem'
